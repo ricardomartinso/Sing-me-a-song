f7e44ed15a2d073ca2d51b1e7550ba005d8df31d
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-09-24 18:03:56 -0300

    test: create e2e test route

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -3,10 +3,18 @@ import express from "express";
 import "express-async-errors";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
 import recommendationRouter from "./routers/recommendationRouter.js";
+import dotenv from "dotenv";
+import e2eRouter from "./routers/e2eRouter.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === "test") {
+  app.use(e2eRouter);
+}
 
 app.use("/recommendations", recommendationRouter);
 app.use(errorHandlerMiddleware);
